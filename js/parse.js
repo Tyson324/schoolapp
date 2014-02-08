df1355e48c39a7fc03e08ff38ebd91daae2106a2
Tyson324 <sgonez@hotmail.com>
2014-02-08 21:32:54 +0000

    more experiments
    man oh man is jqmobile confusing...

--- js/parse.js
+++ js/parse.js
@@ -6,95 +6,43 @@
 // Init Parse
 Parse.initialize("gRgG6rXH59W6J19MrqWtiAWAdEGqjoZjEs0GsH6H", "viwxKM3bX10rDDpGpdc3U0kDCr9eJm4WDhhrlPR3");
 
+var logIn = function(event){
+	event.preventDefault();
+	SignIn();
+};
 
+$("#signInBtn").click(function(event){
+            event.preventDefault();
+            signIn();
+        });
+   
+        
+        function signIn(){
+            
+            var username = document.getElementById("usrID").value;
+            var password = document.getElementById("pass").value;
 
+           Parse.User.logIn(username, password, {
+          success: function(user) {
+            alert("Your'e signed in now.");
+            goHome();
+          },
 
-	
-	
+          error: function(error) {
+            function error() {
+            console.log(error.message);
+            };
+          }
+            });   
+        };
+       // End function to sign in
+       //Function to go to home page
+       
+       function goHome (){
+        $('body').fadeOut( 1000, homeRedir());
+       };
+       function homeRedir (){
+        window.location.replace("#Home")
+       };
+       //End function 
 
-	
-
-
-
-	// Log In
-
-	var logIn = function () {
-		
-		// Get values
-		var $username = $("#userID"),
-		    $password = $("#pass");
-
-		    
-		var username = $username.val(),
-		    password = $password.val();
-			
-
-
-		// Validate
-		
-		if (username.length == 0) {
-
-			alert("You havent entered a username!");			
-
-			return false;
-		}
-
-		else if (password.length == 0) {
-			alert("you havent entered a password!");			
-
-			return false;
-		}
-
-
-
-		Parse.User.logIn(username, password, {
-			success: function(user) {
-
-				$.mobile.changePage('#home');
-
-				if (buildIt()) {
-					console.log("loaded build from success!");
-				}
-
-					
-			},
-
-			error: function(user, error) {
-				if (error.code == 101) {
-
-					
-					alert("Check your username and password and try again!");
-					
-					return false;
-					
-				}
-
-				else {
-					alert("Error: " + error.code + " " + error.message);
-					return false;
-				}
-			}
-		});
-	}
-
-
-	
-
-
-	function logOut() {
-		Parse.User.logOut();
-		window.location.href = 'index.html#login';
-
-		alert("You have been logged out")
-		buildMenu();
-	}
-
-
-	function checkloggedin (){
-		currentUser = Parse.User.current();
-		if (currentUser == null) {
-
-			alert("You must be logged in to continue.");
-			return false;
-		}
-	}
